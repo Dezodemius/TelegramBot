e73d806873531344a23c0ca5366be69e2fa9776c
Егор <gladkovyegor@gmail.com>
2022-07-16 14:22:33 +0400

    init program

---
+++ TelegramBot/TelegramBot/Program.cs
@@ -0,0 +1,13 @@
+﻿using TelegramBot.Configuration;
+
+namespace TelegramBot
+{
+  public static class Program
+  {
+    public static void Main(string[] args)
+    {
+      var token = new BotConfigManager().GetSettings().Token;
+      var botManager = new TelegramBotManager(token);
+    }
+  }
+}

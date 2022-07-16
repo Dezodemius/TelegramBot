171a56a07e92eb511fb22a68b46431726f75b76a
Егор <gladkovyegor@gmail.com>
2022-07-16 14:22:26 +0400

    init telegram manager

---
+++ TelegramBot/TelegramBot/TelegramBotManager.cs
@@ -0,0 +1,13 @@
+﻿using Telegram.Bot;
+
+namespace TelegramBot;
+
+public class TelegramBotManager
+{
+  private readonly TelegramBotClient client;
+
+  public TelegramBotManager(string token)
+  {
+    this.client = new TelegramBotClient(token);
+  }
+}

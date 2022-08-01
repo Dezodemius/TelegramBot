9e472f6baa65f0216b2c1c44743371ba8d0d7f03
Егор <gladkovyegor@gmail.com>
2022-08-01 20:05:44 +0400

    Refactor App Settings

---
+++ TelegramBot/Common/Settings/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace TelegramBot.Configuration;
+
+/// <summary>
+/// Настройки приложения.
+/// </summary>
+public class AppSettings
+{
+  /// <summary>
+  /// Токен телеграм-бота.
+  /// </summary>
+  public string Token { get; set; }
+
+  /// <summary>
+  /// Строка подключения к БД.
+  /// </summary>
+  public string SqlConnectionString { get; set; }
+}
---
+++ TelegramBot/Common/Settings/AppSettingsManager.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace TelegramBot.Configuration;
+
+/// <summary>
+/// Менеджер конфига приложения.
+/// </summary>
+public static class AppSettingsManager
+{
+  /// <summary>
+  /// Настройки из конфига.
+  /// </summary>
+  public static AppSettings Settings { get; }
+
+  /// <summary>
+  /// Имя файла с конфигом.
+  /// </summary>
+  private const string CONFIG_FILENAME = "botSettings.yml";
+
+  /// <summary>
+  /// Файл с конфигом.
+  /// </summary>
+  private static readonly FileInfo configFile = new FileInfo(CONFIG_FILENAME);
+
+  /// <summary>
+  /// Получить настройки из файла.
+  /// </summary>
+  /// <returns>Настройки.</returns>
+  private static AppSettings GetSettings()
+  {
+    var deserializer = new YamlDotNet.Serialization.DeserializerBuilder()
+      .IgnoreUnmatchedProperties()
+      .WithNamingConvention(CamelCaseNamingConvention.Instance)
+      .Build();
+    return deserializer.Deserialize<AppSettings>(File.ReadAllText(configFile.FullName));
+  }
+
+  static AppSettingsManager()
+  {
+    Settings = GetSettings();
+  }
+}

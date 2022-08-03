aa7973202f51adb56cdd7099bb00ce5222d78777
Гладков Егор (Gladkov_ES) <gladkov_es@directum.ru>
2022-08-03 13:52:00 +0400

    Bot commands

---
+++ TelegramBot/TelegramBot/BotCommand.cs
@@ -0,0 +1,37 @@
+﻿namespace TelegramBot;
+
+/// <summary>
+/// Команды бота.
+/// </summary>
+public static class BotCommand
+{
+  /// <summary>
+  /// Страт.
+  /// </summary>
+  public const string Start = "/start";
+
+  /// <summary>
+  /// Вызов справки.
+  /// </summary>
+  public const string Help = "/help";
+
+  /// <summary>
+  /// О боте.
+  /// </summary>
+  public const string About = "/about";
+
+  /// <summary>
+  /// Добавить студента.
+  /// </summary>
+  public const string AddStudent = "/addstudent";
+
+  /// <summary>
+  /// Добавить группу.
+  /// </summary>
+  public const string AddGroup = "/addgroup";
+
+  /// <summary>
+  /// Добавить домашнее задание.
+  /// </summary>
+  public const string AddHomework = "/addhomework";
+}
